984d973364b67da428f09a3ea6c832f287b5e709
mercy <mercymugambi2016@gmail.com>
2022-10-07 18:06:32 +0300

    Reverse array Challenge

---
+++ ReverseArrayChallenge.py
@@ -0,0 +1,10 @@
+#Challenge quest - easy category - leetcode
+#Write a Function/method  ReverseArray(a) that takes and array of integers and reverses it
+#e.g [1,4,5,6,9,8]->[8,9,6,5,4,1]
+
+#solution
+
+a = [1,4,5,6,9,8]
+x = a.reverse()
+print(a)
+
